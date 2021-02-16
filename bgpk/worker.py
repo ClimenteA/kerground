4a5ba29bf978df619df89b9d5cb1ce3ff868503b
ClimenteA <climente.alin@gmail.com>
2021-02-16 18:26:11 +0200

    using importlib we can recreate the environment for running a func in another module

--- bgpk/worker.py
+++ bgpk/worker.py
@@ -3,6 +3,9 @@ import os, shutil
 import tempfile
 import uuid, pickle
 from collections import namedtuple
+
+import inspect
+import sys
 import concurrent.futures as cf
 
 
@@ -17,6 +20,7 @@ class BGPKWorker:
 
     def __init__(self, worker_dir=BASE_DIR):
         self.worker_dir = BGPKWorker.prep_worker_dir(worker_dir)
+        self.worker_file_paths = BGPKWorker.get_worker_file_paths()
         self.con = sqlite3.connect(os.path.join(self.worker_dir, 'tasks.db'))
         self.con.execute("CREATE TABLE IF NOT EXISTS tasks (id TEXT NOT NULL, status TEXT NOT NULL);")
         self.con.commit()
@@ -37,6 +41,22 @@ class BGPKWorker:
             os.mkdir(worker_dir)
 
         return worker_dir
+
+
+    @staticmethod
+    def get_worker_file_paths():
+
+        worker_file_paths = []   
+        for root, dirs, files in os.walk(BASE_DIR): 
+            for file in files:
+                if (
+                    file.endswith("_worker.py")
+                    or
+                    file.startswith("worker_") and file.endswith(".py")
+                ):
+                    worker_file_paths.append(os.path.join(root, file))
+                    
+        return worker_file_paths
 
 
     def save_task(self, task):
@@ -61,7 +81,19 @@ class BGPKWorker:
 
 
 
+worker = BGPKWorker()  
+
+worker_file_path = "/home/acmt/Documents/background-worker/worker/other_folder/another_worker.py"
 
 
-worker = BGPKWorker()       
-        
+
+from importlib.util import spec_from_file_location, module_from_spec
+
+
+module_name = os.path.basename(worker_file_path).split('.py')[0]
+spec_module = spec_from_file_location(module_name, worker_file_path)
+module = module_from_spec(spec_module)
+# Import the module
+spec_module.loader.exec_module(module)
+# Execute a function from that module
+module.another_task()
